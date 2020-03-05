e4073f4f8eb927dc17bec8edafccea7289d6870a
Ivan Notaros <nothke@gmail.com>
2020-03-05 21:40:25 +0100

    Moved jobs to separate file

--- Assets/Scripts/PixelDataTest.cs
+++ Assets/Scripts/PixelDataTest.cs
@@ -5,40 +5,10 @@ using UnityEngine.Profiling;
 
 using Unity.Collections;
 using Unity.Jobs;
-using Unity.Burst;
-
-using Unity.Mathematics;
-using static Unity.Mathematics.math;
 
 public class PixelDataTest : MonoBehaviour
 {
     public Texture2D texture;
-
-    [BurstCompile]
-    struct SetPixelsJob : IJobParallelFor
-    {
-        [WriteOnly] public NativeArray<Color32> colors;
-
-        public void Execute(int i)
-        {
-            colors[i] = new Color32(255, 255, 255, 255);
-        }
-    }
-
-    [BurstCompile]
-    struct SetNoisePixelsJob : IJobParallelFor
-    {
-        [WriteOnly] public NativeArray<Color32> colors;
-
-        public void Execute(int i)
-        {
-            int y = i / SIZE;
-            int x = i % SIZE;
-
-            float2 pos = float2(x, y) * 0.1234f;
-            colors[i] = new Color32((byte)(noise.cnoise(pos) * 256), 0, 0, 255);
-        }
-    }
 
     const int SIZE = 512;
     const int TSIZE = SIZE * SIZE;
@@ -51,14 +21,14 @@ public class PixelDataTest : MonoBehaviour
 
         colors = texture.GetRawTextureData<Color32>();
 
-        new SetPixelsJob() { colors = colors }.Schedule(TSIZE, 512).Complete();
+        new PixelJobs.SetJob() { colors = colors }.Schedule(TSIZE, 512).Complete();
 
         texture.Apply(false);
     }
 
     private void Update()
     {
-        new SetPixelsJob() { colors = colors }.Schedule(TSIZE, 512).Complete();
+        new PixelJobs.SetJob() { colors = colors }.Schedule(TSIZE, 512).Complete();
 
         Profiler.BeginSample("Texture Apply");
         texture.Apply(false);
---
+++ Assets/Scripts/PixelJobs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Burst;
+
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
+
+public static class PixelJobs
+{
+    [BurstCompile]
+    public struct SetJob : IJobParallelFor
+    {
+        [WriteOnly] public NativeArray<Color32> colors;
+        public int size;
+
+        public void Execute(int i)
+        {
+            colors[i] = new Color32(255, 255, 255, 255);
+        }
+    }
+
+    [BurstCompile]
+    public struct SetNoiseJob : IJobParallelFor
+    {
+        [WriteOnly] public NativeArray<Color32> colors;
+        public int size;
+
+        public void Execute(int i)
+        {
+            int y = i / size;
+            int x = i % size;
+
+            float2 pos = float2(x, y) * 0.1234f;
+            colors[i] = new Color32((byte)(noise.cnoise(pos) * 256), 0, 0, 255);
+        }
+    }
+}
